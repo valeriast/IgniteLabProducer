7fb329b563eaddd1de2e9e16419114d2b13567f6
Valeria De Souza Teixeira <valeria.s@outlook.com.br>
2023-01-02 23:13:20 -0300

    Update producer.js

--- producer.js
+++ producer.js
@@ -4,11 +4,11 @@ import { randomUUID } from 'node:crypto'
 async function bootstrap(){
     const kafka = new Kafka({
         clientId: 'notifications',
-        brokers: ['mighty-quetzal-8328-us1-kafka.upstash.io:9092'],
+        brokers: ['##'],
         sasl: {
             mechanism: 'scram-sha-256',
-            username: 'bWlnaHR5LXF1ZXR6YWwtODMyOCTUcz_3R4wjVEodWIJMKxNdP36Egcyt2GKY7nI',
-            password: '200176ba93ad413da2594e3faefaef46',
+            username: '##',
+            password: '##',
         },
         ssl: true,
     })
